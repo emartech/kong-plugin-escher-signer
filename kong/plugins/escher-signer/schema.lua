cf9c5f187adf07dd47e8027a9e9c5654b1776f76
bencso <gabor.bencsik13@gmail.com>
2018-11-06 11:33:54 +0100

    INT-0073: Add schema definition

--- kong/plugins/escher-signer/schema.lua
+++ kong/plugins/escher-signer/schema.lua
@@ -1,6 +1,13 @@
 return {
-  no_consumer = true,
-  fields = {
-    say_hello = { type = "boolean", default = true }
-  }
+    no_consumer = true,
+    fields = {
+        vendor_key = { type = "string", default = "EMS" },
+        algo_prefix = { type = "string", default = "EMS" },
+        hash_algo = { type = "string", default = "SHA256" },
+        auth_header_name = { type = "string", default = "X-EMS-Auth" },
+        date_header_name = { type = "string", default = "X-EMS-Date" },
+        access_key_id = { type = "string", required = true },
+        api_secret = { type = "string", required = true },
+        credential_scope = { type = "string", required = true }
+    }
 }
--- spec/escher-signer/plugin_e2e_spec.lua
+++ spec/escher-signer/plugin_e2e_spec.lua
@@ -3,81 +3,65 @@ local cjson = require "cjson"
 local TestHelper = require "spec.test_helper"
 
 local function get_response_body(response)
-  local body = assert.res_status(201, response)
-  return cjson.decode(body)
+    local body = assert.res_status(201, response)
+    return cjson.decode(body)
 end
 
 local function setup_test_env()
-  helpers.dao:truncate_tables()
+    helpers.dao:truncate_tables()
 
-  local service = get_response_body(TestHelper.setup_service('test-service', 'http://mockbin:8080/request'))
-  local route = get_response_body(TestHelper.setup_route_for_service(service.id, '/anything'))
-  local plugin = get_response_body(TestHelper.setup_plugin_for_service(service.id, 'escher-signer'))
-  local consumer = get_response_body(TestHelper.setup_consumer('TestUser'))
-  return service, route, plugin, consumer
+    local service = get_response_body(TestHelper.setup_service('test-service', 'http://mockbin:8080/request'))
+    local route = get_response_body(TestHelper.setup_route_for_service(service.id, '/anything'))
+    local plugin = get_response_body(TestHelper.setup_plugin_for_service(service.id, 'escher-signer'))
+    local consumer = get_response_body(TestHelper.setup_consumer('TestUser'))
+    return service, route, plugin, consumer
 end
 
 describe("Plugin: escher-signer (access)", function()
 
-  setup(function()
-    helpers.start_kong({ custom_plugins = 'escher-signer' })
-  end)
-
-  teardown(function()
-    helpers.stop_kong(nil)
-  end)
-
-  describe("Admin API", function()
-    local service, route, plugin, consumer
-
-    before_each(function()
-      service, route, plugin, consumer = setup_test_env()
+    setup(function()
+        helpers.start_kong({ custom_plugins = 'escher-signer' })
     end)
 
-    it("registered the plugin globally", function()
-      local res = assert(helpers.admin_client():send {
-        method = "GET",
-        path = "/plugins/" .. plugin.id,
-      })
-      local body = assert.res_status(200, res)
-      local json = cjson.decode(body)
-
-      assert.is_table(json)
-      assert.is_not.falsy(json.enabled)
+    teardown(function()
+        helpers.stop_kong(nil)
     end)
 
-    it("registered the plugin for the api", function()
-      local res = assert(helpers.admin_client():send {
-        method = "GET",
-        path = "/plugins/" ..plugin.id,
-      })
-      local body = assert.res_status(200, res)
-      local json = cjson.decode(body)
-      assert.is_equal(api_id, json.api_id)
+    describe("Escher Signer", function()
+        local service
+
+        before_each(function()
+            helpers.dao:truncate_tables()
+            service = get_response_body(TestHelper.setup_service('test-service', 'http://mockbin:8080/request'))
+        end)
+
+        it("should set default config items on empty config", function()
+            local mock_config = {
+                access_key_id = "dummy_key",
+                api_secret = "dummy_secret",
+                credential_scope = "dummy_credential_scope"
+            }
+            local plugin = get_response_body(TestHelper.setup_plugin_for_service(service.id, 'escher-signer', mock_config))
+
+            local config = plugin.config
+
+            assert.is_equal(config.vendor_key, "EMS")
+            assert.is_equal(config.algo_prefix, "EMS")
+            assert.is_equal(config.hash_algo, "SHA256")
+            assert.is_equal(config.auth_header_name, "X-EMS-Auth")
+            assert.is_equal(config.date_header_name, "X-EMS-Date")
+        end)
+
+        it("should require access_key_id, api_secret and credantial_scope", function()
+            local plugin_response = TestHelper.setup_plugin_for_service(service.id, 'escher-signer', {})
+
+            local body = assert.res_status(400, plugin_response)
+            local plugin = cjson.decode(body)
+
+            assert.is_equal(plugin["config.access_key_id"], "access_key_id is required")
+            assert.is_equal(plugin["config.api_secret"], "api_secret is required")
+            assert.is_equal(plugin["config.credential_scope"], "credential_scope is required")
+        end)
     end)
-  end)
-
-  describe("Response", function()
-    local service, route, plugin, consumer
-
-    before_each(function()
-      service, route, plugin, consumer = setup_test_env()
-    end)
-
-    it("added the header", function()
-      local res = assert(helpers.proxy_client():send {
-        method = "GET",
-        path = "/anything",
-        headers = {
-          ["Host"] = "test1.com"
-        }
-      })
-
-      assert.res_status(200, res)
-      assert.response(res).has.header("Hello-World")
-      local header_value = res.headers["Hello-World"]
-      assert.is_equal("Hey!", header_value)
-    end)
-  end)
 
 end)
