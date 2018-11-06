f45bc8b71f1f230053ca55532cd714c8f748e067
bencso <gabor.bencsik13@gmail.com>
2018-11-06 11:32:57 +0100

    INT-0073: remove boilerplate code

--- kong/plugins/escher-signer/handler.lua
+++ kong/plugins/escher-signer/handler.lua
@@ -10,15 +10,6 @@ end
 
 function EscherSignerHandler:access(conf)
   EscherSignerHandler.super.access(self)
-
-  if conf.say_hello then
-    ngx.log(ngx.ERR, "============ Hey World! ============")
-    ngx.header["Hello-World"] = "Hey!"
-  else
-    ngx.log(ngx.ERR, "============ Bye World! ============")
-    ngx.header["Hello-World"] = "Bye!"
-  end
-
 end
 
 return EscherSignerHandler
