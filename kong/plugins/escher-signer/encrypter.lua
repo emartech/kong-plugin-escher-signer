0c90ae63ff1ca9dd7a76b9762ee27fdc4a00a633
bencso <gabor.bencsik13@gmail.com>
2018-11-08 15:06:43 +0100

    INT-0073: Add encrypter wrapper

---
+++ kong/plugins/escher-signer/encrypter.lua
@@ -0,0 +1,32 @@
+local Object = require "classic"
+local EasyCrypto = require "resty.easy-crypto"
+
+local Encrypter = Object:extend()
+
+function Encrypter:new(encryption_key)
+    self.encryption_key = encryption_key
+
+    self.crypto = EasyCrypto:new({
+        saltSize = 12,
+        ivSize = 16,
+        iterationCount = 10000
+    })
+end
+
+function Encrypter:encrypt(subject)
+    return self.crypto:encrypt(self.encryption_key, subject)
+end
+
+function Encrypter:decrypt(subject)
+    return self.crypto:decrypt(self.encryption_key, subject)
+end
+
+function Encrypter.create_from_file(file_path)
+    local file = io.open(file_path, "r")
+    local encryption_key = file:read("*all")
+    file:close()
+
+    return Encrypter(encryption_key)
+end
+
+return Encrypter
